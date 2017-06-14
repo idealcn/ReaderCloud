3ff3a676c6a298557fc6acbfb14fb5de348ed642
idealcn <android_gn0610@126.com>
2017-06-15 02:25:03 +0800

    添加左侧侧滑,未完成

--- app/src/main/java/com/idealcn/reader/ui/HomeActivity.java
+++ app/src/main/java/com/idealcn/reader/ui/HomeActivity.java
@@ -1,22 +1,10 @@
 package com.idealcn.reader.ui;
 
-import android.databinding.DataBindingUtil;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 
 import com.idealcn.reader.R;
-import com.idealcn.reader.User;
-import com.idealcn.reader.adapter.home.HomeAdapter;
-import com.idealcn.reader.binding.ActivityHomeBinding;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.idealcn.reader.databinding.ActivityHomeBinding;
 
 /**
  * Created by ideal on 17-6-14.
@@ -31,34 +19,9 @@ public class HomeActivity  extends BaseActivity {
         super.onCreate(savedInstanceState);
         homeBinding = (ActivityHomeBinding) binding;
 
-        homeBinding.homeListview.setLayoutManager(new LinearLayoutManager(this){
-            @Override
-            public RecyclerView.LayoutParams generateDefaultLayoutParams() {
-                RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(
-                        RecyclerView.LayoutParams.MATCH_PARENT,
-                        120
-                );
-                return layoutParams;
-            }
-        });
-        homeBinding.homeListview.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL){
-            @Override
-            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-                super.getItemOffsets(outRect, view, parent, state);
-                outRect.top = 5;
-            }
-        });
+        homeBinding.mainNavView.inflateHeaderView(R.layout.layout_main_nav_view);
+//        homeBinding.mainNavView.setOnClickView((view) -> System.out.println("hello"));
 
-        List<User> list = new ArrayList<>();
-        for (int x = 0; x < 10; x++) {
-            User user = new User("user---"+x);
-            user.phone = "10001---"+x;
-            list.add(user);
-        }
-
-        HomeAdapter homeAdapter = new HomeAdapter(this,list);
-
-        homeBinding.homeListview.setAdapter(homeAdapter);
     }
 
     @Override
